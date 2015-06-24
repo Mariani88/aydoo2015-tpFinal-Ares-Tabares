e5052a007bf689a520521ed12af0fab1a4f2dfea
Fernando Ares <ferares4@gmail.com>
2015-06-23 23:58:23 -0300

    Cambio visibilidad del metodo generarListaEnFormatoYML a protected

--- src/aydoo/tpfinal/Estadistica.java
+++ src/aydoo/tpfinal/Estadistica.java
@@ -8,7 +8,7 @@ public abstract class Estadistica {
 
 	protected abstract List<String> generarEstadistica(List<Recorrido> listaDeRecorridos);
 
-	public abstract List<String> generarListaEnFormatoYML(List<String> lista);
+	protected abstract List<String> generarListaEnFormatoYML(List<String> lista);
 	
 	public String getNombreEstadistica() {
 		return nombreEstadistica;
